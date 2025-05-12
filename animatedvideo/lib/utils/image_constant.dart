f209a776ca5f9a507e4d6e5df5d465e85105d8fe
Gaurav Kumar <GAURAVRK215@GMAIL.COM>
2025-05-12 18:26:42 +0530

    utils

---
+++ animatedvideo/lib/utils/image_constant.dart
@@ -0,0 +1,27 @@
+class ImageConstant {
+  // Image folder path
+  static String imagePath = 'assets/images';
+
+// Leaderboard images
+  // static String imageTeams = 'assets/images/team.png';
+  // static String imgArrowLeft = '$imagePath/img_arrow_left.svg';
+  static String imgArrowleft = '$imagePath/img_arrowleft.svg';
+
+  static String userProfileImg = "$imagePath/user-line.svg";
+  static String imgFire = '$imagePath/img_fire.svg';
+
+  static String imgSearchWhiteA70020x20 =
+      '$imagePath/img_search_white_a700_20x20.svg';
+
+  static String imgMenu = '$imagePath/img_menu.svg';
+
+  static String imgOverflowmenuWhiteA700 =
+      '$imagePath/img_overflowmenu_white_a700.svg';
+
+  static String imgArrowleftBlueGray900 =
+      '$imagePath/img_arrowleft_blue_gray_900.svg';
+  static String bubbles = 'assets/icon/bubbles.svg';
+
+  static String close = 'assets/icon/close.svg';
+  static String fail = 'assets/icon/fail.svg';
+}
